2eb997ecbd2a63eac73ca566b0dc286891ae3df7
Bruno Albuquerque <br.brunoalbuquerque@gmail.com>
2022-06-06 11:47:52 -0300

    Initial commit

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,28 @@
+import ProductRepository from "./../typeorm/repositories/ProductsRepository";
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import redisCache from "@shared/cache/RedisCache";
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError("Product not found");
+    }
+
+    // const redisCache = new RedisCache();
+
+    await redisCache.invalidate("api-vendas-PRODUCT_LIST");
+
+    await productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
